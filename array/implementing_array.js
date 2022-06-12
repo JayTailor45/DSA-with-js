10bcb3693ee80f3e38fc7944ffcc71bf10d58a0e
JayTailor45 <tailorj64@gmail.com>
2022-06-12 18:53:26 +0530

    Implementation of array

---
+++ array/implementing_array.js
@@ -0,0 +1,51 @@
+class MyArray {
+  constructor() {
+    this.data = {};
+    this.length = 0;
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  delete(index) {
+    const item = this.data[index];
+    this._shiftItems(index);
+  }
+
+  _shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
+
+const newArray = new MyArray();
+console.log(newArray.get(0));
+
+newArray.push("yo");
+
+console.log(newArray.push("wsup"));
+
+newArray.push("!");
+
+// newArray.pop();
+
+newArray.delete(0);
+
+console.log(newArray);
