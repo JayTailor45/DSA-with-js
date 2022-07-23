254bd9ee7e8feade955a3e90d64034f8d40c5d06
JayTailor45 <tailorj64@gmail.com>
2022-07-23 10:56:28 +0530

    WIP insert

--- linked_list/01_linked_list.js
+++ linked_list/01_linked_list.js
@@ -40,10 +40,37 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  insert(index, value) {
+    // 1 -> 2 -> 3 -> 4
+    // 1 -> 2 -> 3 -> 99 -> 4
+    const newNode = new Node(value);
+
+    let currentNode = this.head;
+    let currentIndex = 0;
+    while (currentNode) {
+      if (currentIndex == index) {
+        // wip
+      }
+      currentIndex++;
+    }
+  }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
-console.log(myLinkedList);
+console.log(myLinkedList.printList());
+myLinkedList.insert(2, 99);
+console.log(myLinkedList.printList());
